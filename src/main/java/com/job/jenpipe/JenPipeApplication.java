eec5cf7fe2b7b794b1581e57468bbe2dbd102e72
vaibhavwagh123 <vaibhavwagh123@gmail.com>
2021-09-12 17:41:12 +0530

    temp

--- src/main/java/com/job/jenpipe/JenPipeApplication.java
+++ src/main/java/com/job/jenpipe/JenPipeApplication.java
@@ -2,12 +2,19 @@ package com.job.jenpipe;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class JenPipeApplication {
+public class JenPipeApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(JenPipeApplication.class, args);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(JenPipeApplication.class);
 	}
 
 }
