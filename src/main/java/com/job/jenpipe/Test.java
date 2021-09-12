0649edb41eafc9046281ec5189cc842b4e1fc4e2
vaibhavwagh123 <vaibhavwagh123@gmail.com>
2021-09-12 16:58:08 +0530

    path changes
    path changes

--- src/main/java/com/job/jenpipe/Test.java
+++ src/main/java/com/job/jenpipe/Test.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping(value = "job")
+@RequestMapping(value = "/job")
 public class Test {
 
 	@GetMapping(value = "/hello")
