650084cb606aebaab9ff0152ab4276a574256c8d
vaibhavwagh123 <vaibhavwagh123@gmail.com>
2021-09-12 17:05:23 +0530

    path edited

--- src/main/java/com/job/jenpipe/Test.java
+++ src/main/java/com/job/jenpipe/Test.java
@@ -5,7 +5,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping(value = "/job")
 public class Test {
 
 	@GetMapping(value = "/hello")
